07f021f4a9e14fd04e9931b7b5372b90b9ad480a
Misha Behersky <mykhailob@reblaze.com>
2023-05-25 16:53:45 +0200

    Add chapter for the control flow
    Signed-off-by: Misha Behersky <mykhailob@reblaze.com> :womans_hat:

---
+++ control_flow/forrange.rs
@@ -0,0 +1,32 @@
+fn main() {
+    // one()
+    two()
+}
+
+fn one() {
+    for n in 1..=100 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+
+    }
+}
+
+fn two() {
+   let mut names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.iter_mut() {
+        *name = match name {
+            &mut "Ferris" => "There is a rustacean among us!",
+            _ => "Hello",
+        }
+    }
+
+    println!("names: {:?}", names);
+}
---
+++ control_flow/loop.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let mut count = 0u32;
+
+    println!("Let's count untils infinity!");
+
+    loop {
+        count += 1;
+        if count == 3 {
+            println!("three");
+            continue;
+        }
+
+        println!("{}", count);
+
+        if count == 5 {
+            println!("OK, that's enough");
+            break;
+        }
+    }
+}
